4eb629c97ff6196fa88150d29e89774a55a23864
liuxinghui <liuxh@meprint.com>
2021-06-25 11:53:39 +0800

    ReceiverMessage2

--- consumer/src/main/java/com/xingxin/mq/ReceiverMessage2.java
+++ consumer/src/main/java/com/xingxin/mq/ReceiverMessage2.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.rabbitmq.client.Channel;
 import com.xingxin.entity.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -20,16 +21,16 @@ import java.util.Map;
 @Component
 public class ReceiverMessage2 {
 
-//    @RabbitListener(bindings = @QueueBinding(
-//            value = @Queue(value = "msg_queue",durable = "true"),
-//            exchange = @Exchange(name="msg_exchange",durable = "true",type = "topic"),
-//            key = "msg.*"
-//    )
-//    )
-//    @RabbitHandler
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "msg_queue",durable = "true"),
+            exchange = @Exchange(name="msg_exchange",durable = "true",type = "topic"),
+            key = "msg.*"
+    )
+    )
+    @RabbitHandler
     public void receiverGetMessage(@Payload Message msg, @Headers Map<String,Object> headers, Channel channel) throws IOException {
 
-        log.info("ReceiverMessage1 get message, msg is : {}", JSON.toJSONString(msg));
+        log.info("=====ReceiverMessage1 get message, msg is : {}", JSON.toJSONString(msg));
 
         Long deliveryTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
 
